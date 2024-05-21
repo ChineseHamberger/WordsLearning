987a8a35cdfefa744dbe02a64e1115a18bd6af37
nesohc <mingchen_y@163.com>
2024-05-21 13:07:39 +0800

    V0521-ymc

--- src/main/java/ymc/translator/translator.java
+++ src/main/java/ymc/translator/translator.java
@@ -13,12 +13,14 @@ public class translator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("请选择模式（1，中译英；2，英译中）: ");
+        int mode = Integer.parseInt(scanner.nextLine());
         System.out.print("请输入要翻译的单词: ");
         String query = scanner.nextLine();
         scanner.close();
 
         try {
-            String translation = translate(query);
+            String translation = translate(query, mode);
             System.out.println("翻译结果: " + translation);
         } catch (Exception e) {
             e.printStackTrace();
@@ -26,12 +28,23 @@ public class translator {
         }
     }
 
-    private static String translate(String query) throws IOException {
+    private static String translate(String query, int mode) throws IOException {
         String encodedQuery = URLEncoder.encode(query, StandardCharsets.UTF_8);
-        String url = "http://www.youdao.com/w/eng/" + encodedQuery + "/#keyfrom=dict2.index";
+        String url;
+        if (mode == 1) { // 中译英
+            url = "http://www.youdao.com/w/zh-CHS/" + encodedQuery + "/#keyfrom=dict2.index";
+        } else { // 英译中
+            url = "http://www.youdao.com/w/eng/" + encodedQuery + "/#keyfrom=dict2.index";
+        }
 
         Document doc = Jsoup.connect(url).get();
-        Element translationElement = doc.selectFirst(".trans-container > ul > li");
+        Element translationElement;
+
+        if (mode == 1) { // 中译英
+            translationElement = doc.selectFirst(".wt-container > div:first-child");
+        } else { // 英译中
+            translationElement = doc.selectFirst(".trans-container > ul > li");
+        }
 
         if (translationElement != null) {
             return translationElement.text();
@@ -40,3 +53,4 @@ public class translator {
         }
     }
 }
+
